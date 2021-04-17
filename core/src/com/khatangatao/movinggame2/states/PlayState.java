ae439a8e77a31a833ec28c2f524dbdf8923ce3f2
Evgeny Ovchinnikov <e.ovchinnikov@sysolutions.ru>
2021-04-17 16:53:31 +0700

    Player now can move

--- core/src/com/khatangatao/movinggame2/states/PlayState.java
+++ core/src/com/khatangatao/movinggame2/states/PlayState.java
@@ -1,34 +1,70 @@
 package com.khatangatao.movinggame2.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.khatangatao.movinggame2.Moving;
+import com.khatangatao.movinggame2.sprites.Player;
 
 
 public class PlayState extends State{
-    private Texture player;
+    private Player player;
 
     public PlayState(GameStateManager gameStateManager) {
         super(gameStateManager);
-        player = new Texture("player.png");
+        player = new Player(50, 100);
         camera.setToOrtho(false, Moving.WIDTH/2, Moving.HEIGHT/2);
     }
 
     @Override
     protected void handleInput() {
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            player.getPosition().y += 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            player.getPosition().y -= 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            player.getPosition().x -= 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            player.getPosition().x += 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        // make sure the player stays within the screen bounds
+        if (player.getPosition().x < 0) {
+            player.getPosition().x = 0;
+        }
+
+        if (player.getPosition().x > camera.viewportWidth - 30) {
+            player.getPosition().x = camera.viewportWidth - 30;
+        }
+
+        if (player.getPosition().y < 0) {
+            player.getPosition().y = 0;
+        }
+
+        if (player.getPosition().y > camera.viewportHeight - 30) {
+            player.getPosition().y = camera.viewportHeight - 30;
+        }
 
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
+        player.update(dt);
     }
 
     @Override
     public void render(SpriteBatch spriteBatch) {
         spriteBatch.setProjectionMatrix(camera.combined);
         spriteBatch.begin();
-        spriteBatch.draw(player, 50,50);
+        spriteBatch.draw(player.getTexture(), player.getPosition().x,player.getPosition().y);
         spriteBatch.end();
 
     }
