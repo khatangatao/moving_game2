05ede67c52b3de1509a54186488f622f126f5c98
e.ovchinnikov <khatangatao@gmail.com>
2021-07-18 22:21:56 +0700

    Work on music, soundeffects, dialog...

--- core/src/com/khatangatao/movinggame2/states/GameOverState.java
+++ core/src/com/khatangatao/movinggame2/states/GameOverState.java
@@ -2,19 +2,28 @@ package com.khatangatao.movinggame2.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.NinePatch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.khatangatao.movinggame2.Moving;
 
-public class GameOverState extends State{
+public class GameOverState extends State {
     private Texture background;
     private Texture playBtn;
     private Music levelMusic;
+    private Dialog dialog;
+    NinePatch patch;
+    TextButton button;
 
-    public GameOverState(GameStateManager gameStateManager){
+    public GameOverState(GameStateManager gameStateManager) {
         super(gameStateManager);
 
         gamePort = new FitViewport(Moving.VIEWPORTWIDTH, Moving.VIEWPORTHEIGHT, camera);
@@ -26,11 +35,22 @@ public class GameOverState extends State{
         levelMusic = Gdx.audio.newMusic(Gdx.files.internal("sfx/gameover.wav"));
         levelMusic.setVolume(1);
         levelMusic.play();
+
+        // Create text button
+        patch = new NinePatch(new Texture(Gdx.files.internal("nine/knob.png")), 12, 12, 12, 12);
+        TextButton.TextButtonStyle style = new TextButton.TextButtonStyle(
+                new NinePatchDrawable(patch),
+                new NinePatchDrawable(patch),
+                new NinePatchDrawable(patch),
+                new BitmapFont()
+        );
+        style.fontColor = new Color(0.3f, 0.2f, 0.8f, 1f);
+        button = new TextButton("hello world", style);
     }
 
     @Override
     protected void handleInput(float dt) {
-        if(Gdx.input.justTouched()) {
+        if (Gdx.input.justTouched()) {
             gameStateManager.set(new PlayState(gameStateManager));
         }
     }
@@ -46,11 +66,11 @@ public class GameOverState extends State{
         spriteBatch.begin();
         // Нарисуй мне часть картинки background, которая поместится в камеру
         spriteBatch.draw(background, camera.position.x - (camera.viewportWidth / 2), 0);
-        spriteBatch.draw(playBtn, Moving.VIEWPORTWIDTH / 2 - playBtn.getWidth()/2, Moving.VIEWPORTHEIGHT / 8);
+        spriteBatch.draw(playBtn, Moving.VIEWPORTWIDTH / 2 - playBtn.getWidth() / 2, Moving.VIEWPORTHEIGHT / 8);
 
-        Skin skin = new Skin(Gdx.files.internal("dialog/uiskin.json"));
-        // fire up the dialog and voila :)
-        Dialog d = new Dialog("Title", skin);
+
+//        dialog.draw(spriteBatch, 1);
+        button.draw(spriteBatch, 1);
 
         spriteBatch.end();
     }
